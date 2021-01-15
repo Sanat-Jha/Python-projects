a5269122b88a0d1eb7ba1a3b9642b41cbe5a34e1
sanat <sanatjha4@gmail.com>
2021-01-15 11:55:13 +0530

    Main

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weunderstandyou.io"
+url = "https://www.google.com/search?rlz=1C1CHBF_enIN869IN870&sxsrf=ALeKk011IoOJxmEJhIsYAXVi2RR9FHicsQ%3A1609999516397&ei=nKT2X7HrF8ev9QO1rruICQ&q=meaning+of+coding&oq=meaning+of+coding&gs_lcp=CgZwc3ktYWIQAzINCAAQyQMQkQIQRhD5ATICCAAyAggAMgcIABAUEIcCMgIIADICCAAyAggAMgIIADICCAAyAggAOgQIABBHOgkIIxAnEEYQ-QE6CAgAEMkDEJECOgUIABCRAjoFCAAQsQM6CwgAELEDEMkDEJECUN0jWOAoYLoqaABwAngAgAHoAogBmwmSAQUyLTMuMZgBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=psy-ab&ved=0ahUKEwixqY3Kk4nuAhXHV30KHTXXDpEQ4dUDCA0&uact=5"
 r = requests.get(url)
 htmlContent = r.content
 soup = BeautifulSoup(htmlContent, "html.parser")
-print(soup.prettify)
+print(soup.findAll('a'))
